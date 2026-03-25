c1a790e0e27a6da0547501f76d4ed000b3b416f5
Christopher Mutono <christopher.mutono@dominodatalab.com>
2026-03-25 22:49:12 +0000

    fix: use CamelCase SetColumn() — snake_case alias does not exist in arrow R pkg

--- R/dataframe.R
+++ R/dataframe.R
@@ -919,7 +919,7 @@ enable_sql_debug <- function(client, datasource, enabled = TRUE, override = list
       }
     )
     if (!is.null(casted)) {
-      tbl <- tbl$set_column(i, tbl$schema$field(i)$name, casted)
+      tbl <- tbl$SetColumn(i, tbl$schema$field(i)$name, casted)
     }
   }
   tbl
